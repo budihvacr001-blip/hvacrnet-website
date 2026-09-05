b5f7c940088455f5fc2224a18e06bbe2e6c7b7f2
user627387846 <1523302220958247-user627387846@noreply.coze.cn>
2026-09-05 09:54:31 +0800

    fix: 在 App.tsx 布局中渲染 Footer 组件，使页脚备案号可见
    Coze-Commit-Type: user
    Coze-User-ID: 1523302220958247
    Coze-Conversation-ID: 100890045997826

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Products from './pages/Products'
@@ -39,6 +40,7 @@ export default function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </HelmetProvider>
   )
