c3d7292f82e9cd5d83ce3befbd9fd406322b0bb1
user627387846 <1523302220958247-user627387846@noreply.coze.cn>
2026-09-15 21:33:18 +0800

    style: Footer 三栏改为严格等距对称布局(grid + 等值gap)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { navLinks } from '../data/navLinks'
 export default function Footer() {
   return (
     <footer className="bg-navy-dark">
-      <div className="px-[5vw] py-12">
-        <div className="flex flex-col gap-10 md:flex-row md:items-start md:justify-between">
+      <div className="px-10 py-12">
+        <div className="grid gap-10 md:grid-cols-3 md:items-start">
           {/* Left - Brand */}
           <div>
             <Link to="/" className="inline-block">
